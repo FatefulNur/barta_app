469753dabf0acf44b0988476abfdbfc9fc768650
FatefulNur <km.nurunnabi66@gmail.com>
2023-12-28 21:42:52 +0600

    Added kebab-case for app name constant

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,10 +10,10 @@ import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 window.Alpine = Alpine;
 Alpine.start();
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const app_name = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
+    title: (title) => `${title} - ${app_name}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })

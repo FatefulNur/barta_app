aac91bc117785345d6523063ea69839a1709f626
FatefulNur <km.nurunnabi66@gmail.com>
2024-01-03 12:07:55 +0600

    Added like resource

---
+++ app/Http/Resources/LikeResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class LikeResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'user' => UserResource::make($this->whenLoaded('user')),
+        ];
+    }
+}

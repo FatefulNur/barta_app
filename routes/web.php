26ea4eab8653321bb8e6996001c354d0af16b71b
FatefulNur <km.nurunnabi66@gmail.com>
2024-01-03 12:16:31 +0600

    Removed unwanted likes routes

--- routes/web.php
+++ routes/web.php
@@ -4,21 +4,22 @@ use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LikeController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SearchController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])->name('register');
-    Route::post('register', [RegisteredUserController::class, 'store']);
+    Route::get('/register', [RegisteredUserController::class, 'create'])->name('register');
+    Route::post('/register', [RegisteredUserController::class, 'store']);
 
     Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('login');
     Route::post('/login', [AuthenticatedSessionController::class, 'store']);
 });
 
 Route::middleware('auth')->group(function () {
-    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
+    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 
     Route::get('/', HomeController::class)->name('home');
 
@@ -26,8 +27,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/users/{user}/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::put('/users/profile/update', [ProfileController::class, 'update'])->name('profile.update');
 
-    Route::resource('posts', PostController::class)->except('index', 'create');
+    Route::resource('posts', PostController::class)->except(['index', 'create']);
     Route::resource('posts.comments', CommentController::class)->except(['index', 'create', 'show'])->scoped();
 
     Route::get('/search', SearchController::class)->name('search');
+
+    Route::resource('likes', LikeController::class)->only(['store', 'destroy']);
 });

41330780a2c74d1a13a38020ae79529fa57e9b4b
FatefulNur <km.nurunnabi66@gmail.com>
2024-01-03 12:13:29 +0600

    Updated navigation views logic

--- resources/js/Layouts/Partials/Navigation.vue
+++ resources/js/Layouts/Partials/Navigation.vue
@@ -1,48 +1,78 @@
 <template>
-    <div class="relative ml-3" x-data="{ open: false }">
-        <div>
-            <button x-on:click="open = !open" type="button"
-                class="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
-                id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                <span class="sr-only">Open user menu</span>
-                <img class="h-8 w-8 rounded-full" :src="auth_user_profile" alt="AVATAR">
-            </button>
-        </div>
+    <nav x-data="{ mobileMenuOpen: false, userMenuOpen: false }" class="bg-white shadow">
+        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div class="flex h-16 justify-between">
+                <div class="flex">
+                    <div class="flex flex-shrink-0 items-center">
+                        <a :href="route('home')">
+                            <h2 class="font-bold text-2xl">{{ $page.props.app_name }}</h2>
+                        </a>
+                    </div>
 
-        <!-- Dropdown menu -->
-        <div x-show="open" x-on:click.away="open = false"
-            class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-            role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
-            <a :href="route('profile.index', auth.user.id)" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
-            <a :href="route('profile.edit', auth.user.id)" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                role="menuitem" tabindex="-1" id="user-menu-item-1">Edit Profile</a>
-            <form :action="route('logout')" method="POST" role="menuitem" tabindex="-1" id="user-menu-item-2">
-                <input type="hidden" name="_token" :value="xsrf_token">
-                <button type="submit" class="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Sign
-                    out</button>
-            </form>
-        </div>
-    </div>
-    <div class="-mr-2 flex items-center sm:hidden">
-        <!-- Mobile menu button -->
-        <button x-on:click="mobileMenuOpen = !mobileMenuOpen" type="button"
-            class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500"
-            aria-controls="mobile-menu" aria-expanded="false">
-            <span class="sr-only">Open main menu</span>
-            <!-- Icon when menu is closed -->
-            <svg x-show="!mobileMenuOpen" class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none"
-                viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-            </svg>
+                </div>
 
-            <!-- Icon when menu is open -->
-            <svg x-show="mobileMenuOpen" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-            </svg>
-        </button>
-    </div>
+                <!-- Search Input -->
+                <form :action="route('search')" method="GET" class="flex items-center">
+                    <input type="text" name="q" placeholder="Search..."
+                        class="border-2 border-gray-300 bg-white h-10 px-5 pr-10 rounded-full text-sm focus:outline-none"
+                        :value="$page.props.search_query">
+                </form>
+
+                <!-- Navigation Menu -->
+                <div class="hidden sm:ml-6 sm:flex gap-2 sm:items-center">
+                    <div class="relative ml-3" x-data="{ open: false }">
+                        <div>
+                            <button x-on:click="open = !open" type="button"
+                                class="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
+                                id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                                <span class="sr-only">Open user menu</span>
+                                <img class="h-8 w-8 rounded-full" :src="auth_user_profile" alt="AVATAR">
+                            </button>
+                        </div>
+
+                        <!-- Dropdown menu -->
+                        <div x-show="open" x-on:click.away="open = false"
+                            class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                            role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+                            <a :href="route('profile.index', auth.user.id)"
+                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem"
+                                tabindex="-1" id="user-menu-item-0">Your Profile</a>
+                            <a :href="route('profile.edit', auth.user.id)"
+                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem"
+                                tabindex="-1" id="user-menu-item-1">Edit Profile</a>
+                            <form :action="route('logout')" method="POST" role="menuitem" tabindex="-1"
+                                id="user-menu-item-2">
+                                <input type="hidden" name="_token" :value="xsrf_token">
+                                <button type="submit"
+                                    class="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Sign
+                                    out</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="-mr-2 flex items-center sm:hidden">
+                    <!-- Mobile menu button -->
+                    <button x-on:click="mobileMenuOpen = !mobileMenuOpen" type="button"
+                        class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500"
+                        aria-controls="mobile-menu" aria-expanded="false">
+                        <span class="sr-only">Open main menu</span>
+                        <!-- Icon when menu is closed -->
+                        <svg x-show="!mobileMenuOpen" class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none"
+                            viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                        </svg>
+
+                        <!-- Icon when menu is open -->
+                        <svg x-show="mobileMenuOpen" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </nav>
 </template>
 
 <script setup>
--- resources/views/layouts/partials/navigation.blade.php
+++ resources/views/layouts/partials/navigation.blade.php
@@ -1,37 +1,39 @@
-<div class="relative ml-3" x-data="{ open: false }">
-    <div>
-        <button @click="open = !open" type="button"
-            class="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
-            id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-            <span class="sr-only">Open user menu</span>
-            <img class="h-8 w-8 rounded-full"
-                src="{{ auth()->user()->getFirstMediaUrl(App\Constants\MediaCollectionName::PROFILE_IMAGE) }}"
-                alt="AVATAR">
-        </button>
-    </div>
+<div class="hidden sm:ml-6 sm:flex gap-2 sm:items-center">
+    <div class="relative ml-3" x-data="{ open: false }">
+        <div>
+            <button @click="open = !open" type="button"
+                class="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
+                id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                <span class="sr-only">Open user menu</span>
+                <img class="h-8 w-8 rounded-full"
+                    src="{{ auth()->user()->getFirstMediaUrl(App\Constants\MediaCollectionName::PROFILE_IMAGE) }}"
+                    alt="AVATAR">
+            </button>
+        </div>
 
-    <!-- Dropdown menu -->
-    <div x-show="open" @click.away="open = false"
-        class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-        role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
-        <a href="{{ route('profile.index', auth()->id()) }}"
-            class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem" tabindex="-1"
-            id="user-menu-item-0">Your Profile</a>
-        <a href="{{ route('profile.edit', auth()->id()) }}"
-            class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem" tabindex="-1"
-            id="user-menu-item-1">Edit Profile</a>
-        <form action="{{ route('logout') }}" method="POST" role="menuitem" tabindex="-1" id="user-menu-item-2">
-            @csrf
+        <!-- Dropdown menu -->
+        <div x-show="open" @click.away="open = false"
+            class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+            role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+            <a href="{{ route('profile.index', auth()->id()) }}"
+                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem" tabindex="-1"
+                id="user-menu-item-0">Your Profile</a>
+            <a href="{{ route('profile.edit', auth()->id()) }}"
+                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem" tabindex="-1"
+                id="user-menu-item-1">Edit Profile</a>
+            <form action="{{ route('logout') }}" method="POST" role="menuitem" tabindex="-1" id="user-menu-item-2">
+                @csrf
 
-            <button type="submit" class="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Sign
-                out</button>
-        </form>
+                <button type="submit"
+                    class="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Sign
+                    out</button>
+            </form>
+        </div>
     </div>
-</div>
 </div>
 <div class="-mr-2 flex items-center sm:hidden">
     <!-- Mobile menu button -->
-    <button @click="mobileMenuOpen = !mobileMenuOpen" type="button"
+    <button x-on:click="mobileMenuOpen = !mobileMenuOpen" type="button"
         class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500"
         aria-controls="mobile-menu" aria-expanded="false">
         <span class="sr-only">Open main menu</span>

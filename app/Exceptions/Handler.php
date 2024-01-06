5fd0f3f12d9c38677619f79979b637c910f90eb4
FatefulNur <km.nurunnabi66@gmail.com>
2024-01-06 11:40:37 +0600

    Added 404 page logic using vue

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,8 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -25,6 +27,12 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+
+        $this->renderable(function (Throwable $e, Request $request) {
+            if ($e instanceof NotFoundHttpException) {
+                return inertia()->render('Error', ['status' => $e->getStatusCode()]);
+            }
         });
     }
 }
---
+++ resources/js/Pages/Error.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({ status: Number })
+
+const title = computed(() => {
+    return {
+        404: 'Page Not Found',
+    }[props.status]
+})
+
+const description = computed(() => {
+    return {
+        404: 'Sorry, the page you are looking for could not be found.',
+    }[props.status]
+})
+</script>
+
+<template>
+    <div class="flex items-center justify-center min-h-screen flex-col bg-slate-200 gap-2">
+        <h1 class=" text-3xl text-gray-500 font-semibold">{{ title }}</h1>
+        <div class="text-lg text-gray-500">{{ description }}</div>
+    </div>
+</template>
